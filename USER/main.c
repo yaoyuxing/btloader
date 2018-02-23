faec86096f7a5d82adb2c2c3e88b834747888ab0
yaoyuxing <1756664804@qq.com>
2018-02-23 14:31:12 +0800

    btloader修改工程文件

--- USER/main.c
+++ USER/main.c
@@ -6,14 +6,15 @@
 #include "ff.h"
 #include "my_norflash.h"
 #include "interflash.h" 
-#include "uart.h"
-#include "LocalUart.h"
+#include "uart.h" 
 #include "bootloader.h"
 #include "system_info.h"
 #include "stdlib.h"
 #include "ble_update.h"
- 
- 
+#include "oled.h"
+
+
+
 unsigned char gucAppBuf[APP_BUF_MAX];
 TaskHandle_t  StartTask_Handler;
 #define  START_TASK_PRIO   5
@@ -21,55 +22,75 @@ TaskHandle_t  StartTask_Handler;
 void start_task(void *param)
 {
 	 taskENTER_CRITICAL();           //进入临界区
-	#ifdef INTER_FLASH_TEST
+	#if INTER_FLASH_TEST
 	interflash_test();
 	#endif
-	#ifdef NOR_FLASH_FS_TEST
+	#if NOR_FLASH_FS_TEST
 	ceate_fs_test();
 	#endif
-	#ifdef USART_TEST
+	#if USART_TEST
 	usart_test_fun();
-	#endif
-	 
+	#endif 
+  CreateOLEDTask();
 	//CreateLocalUartUpdateTask();
 	CreateBleTask();
-	taskEXIT_CRITICAL();           //进入临界区
-	
-	
-	
+	taskEXIT_CRITICAL();           //退出临界区 
 	vTaskDelete(StartTask_Handler); //删除开始任务
 } 
-int main(void)
+
+
+#define gDebugUartHandle gstUart1.UartHandle  
+/**
+  * @brief  打印串口初始化 
+  * @param  Baud: 设置串口波特率  
+  */
+static void DebugUartInit(unsigned int Baud )
 {  
-	
+	gDebugUartHandle.Instance= USART1;
+	gDebugUartHandle.Init.BaudRate=Baud;
+	gDebugUartHandle.Init.WordLength=UART_WORDLENGTH_8B;   //字长为8位数据格式
+	gDebugUartHandle.Init.StopBits=UART_STOPBITS_1;	    //一个停止位
+	gDebugUartHandle.Init.Parity=UART_PARITY_NONE;		    //无奇偶校验位 
+	gDebugUartHandle.Init.Mode=UART_MODE_TX_RX;		    //
+	HAL_UART_Init( (UART_HandleTypeDef*)&gDebugUartHandle); 
+	//HAL_UART_Receive_IT(&gstUart1.UartHandle,(uint8_t *)&gstUart1.ucRecByte,1);  
+	//BleRxByteITSet(gBleRxByte);
+}
+ 
+int main(void)
+{    
 	SCB->VTOR = FLASH_BASE ; 
-	__set_FAULTMASK(0);// 
+	__set_FAULTMASK(0);//  
 	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);                 //中断分组4 0~15抢占方式
 	MPU_ConfigNOR();
 	Cache_Enable();                 //打开L1-Cache
 	HAL_Init();                     //初始化HAL库    
 	Stm32_Clock_Init(432,25,2,9);   //设置时钟,216Mhz 
-	//delay_init(216);                //延时初始化
+	delay_init(216);                //延时初始化
 	BSP_NOR_Init(); 
-	InterFlash_SetBankMode(SINGLE_BANK_MODE); //设置为single bank mode  	
-	InterFlash_EraseSector(SYSTEM_INIT_INFO_ADDR,NV_SIZE_MAX);         
-	//SystemInfoInit_BtLoader();
-	//RefreshSysInfo(); 
-	gstUpdate.unCurrentAppADDR=0x123124;
+//	InterFlash_SetBankMode(SINGLE_BANK_MODE); //设置为single bank mode  	
+////  InterFlash_EraseSector(SYSTEM_INIT_INFO_ADDR,NV_SIZE_MAX);  
+//	SystemInfoInit_BtLoader(); 
+	UpdateInit();		
+	#if 0
+	gstUpdate.unCurrentAppADDR=USER1_APP_ADDR;
+	 gstUpdate.eAppUpdateStatus=eUSR1_UPDATE_CHECK;
+	SaveSysInfo();
+	RunAPP();
+	#endif
+	btIWDG_Init();
+  DebugUartInit(115200);	 //调试串口初始化 
 	
-	SystemInfoInit_BtLoader();
-	CheckUpdateStatus();	 
-	
+
     //创建开始任务
 	xTaskCreate((TaskFunction_t ) start_task,            //任务函数
 							(const char*    ) "start_task",          //任务名称
-							(uint16_t       )START_STK_SIZE,        //任务堆栈大小
-							(void*          )NULL,                  //传递给任务函数的参数
-							(UBaseType_t    )START_TASK_PRIO,       //任务优先级
-							(TaskHandle_t*  )&StartTask_Handler);   //任务句柄                
-    vTaskStartScheduler();          //开启任务调度 						
+							(uint16_t       )START_STK_SIZE,         //任务堆栈大小
+							(void*          )NULL,                   //传递给任务函数的参数
+							(UBaseType_t    )START_TASK_PRIO,        //任务优先级
+							(TaskHandle_t*  )&StartTask_Handler);    //任务句柄                
+	vTaskStartScheduler();          //开启任务调度 						
 }
-
 
 
 
